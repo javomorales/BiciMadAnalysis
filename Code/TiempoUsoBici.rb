12f9cce02fe872926425d498e97e1980a8ad3da2
JAVIER MORALES <javier@MacBook-Pro-de-JAVIER.local>
2018-12-03 22:09:53 +0100

    Add Tiempo Uso Bicis

--- Code/TiempoUsoBici.rb
+++ Code/TiempoUsoBici.rb
@@ -6,8 +6,7 @@
 # ruby Todo.rb FICHERO_W FICHEROs_R
 #
 # EJEMPLOs:
-# ruby Todo.rb 201704_Usage_Bicimad.json 
-# ruby Todo.rb 201704_Usage_Bicimad.json 201705_Usage_Bicimad.json 201706_Usage_Bicimad.json 201707_Usage_Bicimad.json 201708_Usage_Bicimad.json 201709_Usage_Bicimad.json 201710_Usage_Bicimad.json 201711_Usage_Bicimad.json 201712_Usage_Bicimad.json 201801_Usage_Bicimad.json 201802_Usage_Bicimad.json 201803_Usage_Bicimad.json 201804_Usage_Bicimad.json 201805_Usage_Bicimad.json 201806_Usage_Bicimad.json 201807_Usage_Bicimad.json 201808_Usage_Bicimad.json 201809_Usage_Bicimad.json
+# ruby TiempoUsoBici.rb 201709_Usage_Bicimad.json TiempoUsoBici.csv
 #
 # ---------------------
 
@@ -21,28 +20,16 @@ if ARGV.length == 0
   exit
 end
 
-cont = 0
-
-while (cont < ARGV.length)
-
-  puts "***********************************************"
-  puts "------ Todo -------"
-  puts "--------- #{ARGV[cont]} -----------------"
-  puts "***********************************************"
-  # TiempoUsoBici_201809.csv
-  file_w = File.open("Todo_#{ARGV[cont]}.csv", "a")
+# TiempoUsoBici_201809.csv
+file_w = File.open(ARGV[2], "a")
   
-  file_w.write("unplug_hourTime user_day_code idunplug_station idplug_station idunplug_base idplug_base user_type travel_time ageRange zip_code \n")
-  
-  # 201809_Usage_Bicimad.json
-  File.open(ARGV[cont], "r").each do |line|
+# 201809_Usage_Bicimad.json
+File.open(ARGV[1], "r").each do |line|
 
-    data = JSON.parse(line)
+  data = JSON.parse(line)
 
-    if data["travel_time"] > 3600
-      file_w.write("#{data["unplug_hourTime"]["$date"]} #{data["travel_time"]}\n")
-      puts "#{data["unplug_hourTime"]["$date"]} #{data["travel_time"]}"
-    end
+  if data["travel_time"] > 3600
+    file_w.write("#{data["unplug_hourTime"]["$date"]} #{data["travel_time"]}\n")
+    puts "#{data["unplug_hourTime"]["$date"]} #{data["travel_time"]}"
   end
-
 end
